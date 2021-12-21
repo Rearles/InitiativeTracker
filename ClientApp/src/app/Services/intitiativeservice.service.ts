8c9c56bcfc116de0fe36d340694c8c108b62349d
Rearles <53582049+Rearles@users.noreply.github.com>
2021-12-21 15:45:03 -0700

    added some things to service and component

--- ClientApp/src/app/Services/intitiativeservice.service.ts
+++ ClientApp/src/app/Services/intitiativeservice.service.ts
@@ -1,9 +1,23 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Initiative } from '../Interface/initiative';
 
 @Injectable({
   providedIn: 'root'
 })
 export class IntitiativeserviceService {
 
-  constructor() { }
+  constructor(http: HttpClient) { }
+
+
+  addinitiative(initiative: Initiative) {
+
+  }
+  removeinitiative(initiative: Initiative) {
+
+  }
+  getallinitiative() {
+
+  }
 }
+
